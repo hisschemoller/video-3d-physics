171d30c4f21af28545720c358f651a60a29ab4dd
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-06-07 22:39:41 +0200

    Test with spinning wheel.

--- src/projects/hazumiryokuchi/scene.ts
+++ src/projects/hazumiryokuchi/scene.ts
@@ -1,11 +1,13 @@
 /* eslint-disable max-len */
 /* eslint-disable object-curly-newline */
-import { THREE } from 'enable3d';
+import { ExtendedObject3D, THREE } from 'enable3d';
 import { ProjectSettings, VideoData } from '@app/interfaces';
 import MainScene from '@app/mainscene';
 import createTimeline, { Timeline } from '@app/timeline';
 import { getMatrix4 } from '@app/utils';
 import { createActor, createTweenGroup } from './actor';
+import { createSVG } from './actor-mesh';
+import createTween from '@app/tween';
 
 const PROJECT_PREVIEW_SCALE = 0.25;
 const BPM = 117;
@@ -96,6 +98,7 @@ export default class Scene extends MainScene {
     group.getMesh().add(axesHelper);
 
     await this.createActors(projectSettings, videos, group.getMesh());
+    await this.createPhysics();
 
     this.postCreate();
   }
@@ -111,7 +114,8 @@ export default class Scene extends MainScene {
   async createActors(
     projectSettings: ProjectSettings,
     videos: { [key: string]: VideoData },
-    group: THREE.Group,) {
+    group: THREE.Group,
+  ) {
     const to3d = this.to3d.bind(this);
 
     { // BACKGROUND
@@ -148,5 +152,74 @@ export default class Scene extends MainScene {
       actor.getMesh().receiveShadow = false;
       group.add(actor.getMesh());
     }
+  }
+
+  /**
+   * createPhysics
+   */
+  async createPhysics() {
+    if (this.physics.debug) {
+      this.physics.debug.enable();
+    }
+
+    const DOUBLE_PI = Math.PI * 2;
+    const SVG_WHEEL_SIZE = 1000;
+    const DEPTH = 0.05;
+    const radius = 2;
+    const mass = 1;
+    const x = 0;
+    const y = -1.5;
+    const z = 3;
+    const textureUrl = '../assets/projects/spui/texture-rust4.jpg';
+    const svgPath = '../assets/projects/hazumiryokuchi/wheel1.svg';
+    const svgScale = (radius * 2) / SVG_WHEEL_SIZE;
+    const svgMesh = await createSVG(
+      svgPath,
+      svgScale,
+      undefined,
+      DEPTH,
+    );
+    const geometry = svgMesh.geometry.clone();
+
+    // the canvas should exactly cover the SVG extrude front
+    const sizeVector = new THREE.Vector3();
+    geometry.computeBoundingBox();
+    geometry.boundingBox?.getSize(sizeVector);
+    const wRepeat = (1 / sizeVector.x) * svgScale;
+    const hRepeat = (1 / sizeVector.y) * svgScale * -1;
+
+    const texture = new THREE.TextureLoader().load(textureUrl);
+    texture.offset = new THREE.Vector2(0, 1);
+    texture.repeat = new THREE.Vector2(wRepeat, hRepeat);
+    const material = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.BackSide,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.position.set(-radius, radius, DEPTH * -0.5);
+    const wheel = new ExtendedObject3D();
+    wheel.add(mesh);
+    wheel.position.set(x, y, z);
+    wheel.rotation.x = Math.PI / 2;
+    this.scene.add(wheel);
+    this.physics.add.existing(wheel, {
+      mass,
+      shape: 'mesh',
+    });
+    wheel.body.setCollisionFlags(2); // make it kinematic
+
+    // MOTOR HINGE TWEEN
+    const tween = createTween({
+      delay: 0,
+      duration: PATTERN_DURATION * 0.999,
+      onStart: () => {},
+      onUpdate: (progress: number) => {
+        wheel.rotation.z = progress * DOUBLE_PI;
+        wheel.body.needUpdate = true;
+      },
+    });
+    this.timeline.add(tween);
   }
 }
