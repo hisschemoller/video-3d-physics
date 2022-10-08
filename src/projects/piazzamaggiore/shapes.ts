649f636c4c3fcba9201c551f9ec86320fd621b49
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-10-08 20:18:08 +0200

    Add image texture to extruded shape.

--- src/projects/piazzamaggiore/shapes.ts
+++ src/projects/piazzamaggiore/shapes.ts
@@ -2,11 +2,8 @@ import { THREE } from 'enable3d';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Timeline } from '@app/timeline';
 import createTween from '@app/tween';
-import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
-import { createActor } from './actor';
-import { createSVG } from './actor-mesh';
 import { ProjectSettings } from '@app/interfaces';
-import { getMatrix4 } from '@app/utils';
+import { createSVG } from './actor-mesh';
 
 export interface ShapeArgs {
   gltf: GLTF;
@@ -18,6 +15,31 @@ export interface ShapeArgs {
 }
 
 const TEST_IMAGE = '../assets/projects/test/testimage3d.jpg';
+const WIRE_RADIUS = 0.01;
+const EXTRUDE_DEPTH = 0.03;
+
+async function createShape(
+  projectSettings: ProjectSettings,
+  svgScale: number,
+  svgUrl: string,
+  imgUrl: string,
+) {
+  const { scene } = projectSettings;
+  const texture = new THREE.TextureLoader().load(imgUrl);
+  const mesh = await createSVG(svgUrl, svgScale, texture, EXTRUDE_DEPTH, 0x888888);
+
+  // the texture should exactly cover the SVG extrude front
+  const sizeVector = new THREE.Vector3();
+  mesh.geometry.computeBoundingBox();
+  mesh.geometry.boundingBox?.getSize(sizeVector);
+  const wRepeat = (1 / sizeVector.x) * svgScale;
+  const hRepeat = (1 / sizeVector.y) * svgScale * -1;
+  texture.offset = new THREE.Vector2(0, 1);
+  texture.repeat = new THREE.Vector2(wRepeat, hRepeat);
+
+  mesh.position.set(-4, 5, -2.0);
+  scene.add(mesh);
+}
 
 async function createTwoBlackCircles({
   patternDuration, projectSettings, scene, stepDuration,
@@ -25,19 +47,29 @@ async function createTwoBlackCircles({
   const { timeline } = projectSettings;
   const svgScale = 0.5 / 1024;
   const svgUrl = '../assets/projects/piazzamaggiore/circle2.svg';
+  const svgRadius = 0.22;
 
   const circleLeft = await createSVG(svgUrl, svgScale, undefined, 0.003, 0xaa0000);
-  circleLeft.position.x = 0;
+  circleLeft.position.set(-0.4 - svgRadius - svgRadius, svgRadius, 0);
   circleLeft.castShadow = true;
   circleLeft.receiveShadow = true;
 
   const circleRight = circleLeft.clone();
-  circleRight.position.x = 0.4;
+  circleRight.position.set(0.4, svgRadius, 0);
   circleRight.castShadow = true;
   circleRight.receiveShadow = true;
 
+  const connectWire = new THREE.Mesh(
+    new THREE.CylinderGeometry(WIRE_RADIUS, WIRE_RADIUS, 0.8),
+    new THREE.MeshPhongMaterial({ color: 0x333333 }),
+  );
+  connectWire.rotation.z = Math.PI * 0.5;
+  connectWire.castShadow = true;
+  connectWire.receiveShadow = true;
+
   const group = new THREE.Group();
-  group.position.set(-1.1, 4.0, -2.1);
+  group.position.set(1.1, 4.0, -2.1);
+  group.add(connectWire);
   group.add(circleLeft);
   group.add(circleRight);
   scene.add(group);
@@ -49,11 +81,23 @@ async function createTwoBlackCircles({
     onComplete: () => {},
     onStart: () => {},
     onUpdate: (progress: number) => {
-      group.rotation.y = progress * Math.PI * -4;
+      group.rotation.y = progress * Math.PI * -8;
     },
   }));
 }
 
 export default function createShapes(shapeArgs: ShapeArgs) {
   createTwoBlackCircles(shapeArgs);
+  // createShape(
+  //   shapeArgs.projectSettings,
+  //   8 / 1024,
+  //   '../assets/projects/piazzamaggiore/shape1.svg',
+  //   '../assets/projects/piazzamaggiore/shape1.jpg',
+  // );
+  createShape(
+    shapeArgs.projectSettings,
+    8 / 1024,
+    '../assets/projects/piazzamaggiore/shape2.svg',
+    '../assets/projects/piazzamaggiore/shape2.jpg',
+  );
 }
